2c2af35ee1e3bc07f63dbe7625b0ed29f54ed57e
Sid <siddu@guntupalli.net>
2023-08-11 11:51:23 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,25 +1,36 @@
 import tkinter as tk
 from tkinter import filedialog
+from llama_index import VectorStoreIndex, SimpleDirectoryReader
+
+# Declare the index variable globally
+index = None
 
 def browse_file():
+    global index  # Access the global index variable
     file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
     file_path_var.set(file_path)
     file_path_label.config(text=file_path)
 
 def submit_question():
     try:
+        global index  # Declare the global index variable here
         question = question_text.get("1.0", "end-1c")
         question_var.set(question)
         answer_text.delete("1.0", tk.END)  # Clear previous answer
 
-        # Retrieve the stored document using the file_path_var
-        file_path = file_path_var.get()
-        if file_path:
-            with open(file_path, "r", encoding="latin-1") as file:
-                content = file.read()
-                answer_text.insert(tk.END, content)
+        # Index the file if not already indexed
+        if index is None and file_path_var.get():
+            file_path = file_path_var.get()
+            documents = SimpleDirectoryReader(file_path).load_data()
+            index = VectorStoreIndex.from_documents(documents)
+            answer_text.insert(tk.END, "File indexed successfully.\n\n")
+
+        if index is not None:  # Check if the index is initialized
+            query_engine = index.as_query_engine()
+            response = query_engine.query(question)
+            answer_text.insert(tk.END, response)
         else:
-            answer_text.insert(tk.END, "Please select a file.")
+            answer_text.insert(tk.END, "Please select a file and submit the question.")
     except Exception as e:
         answer_text.insert(tk.END, "An error occurred: " + str(e))
 
